01a80099ac6de7faee82ace1a242e0b95cdd0200
AmelieLoulergue <loulergueamelie@yahoo.fr>
2021-12-04 15:38:09 +0100

    merge conflict

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,19 +9,18 @@ function Home() {
   const [thi, setThi]= useState("")
   const [temp, setTemp]= useState("")
   const [humidity, setHumidity]= useState("")
+  const [date, setDate]= useState("")
+<<<<<<< Updated upstream
 
   const MyInput = (props) => <input {...props} placeholder="Où se situe votre exploitation ?" />
+=======
+>>>>>>> Stashed changes
 
   const onSelected = (result) => {
-    console.log("selected", result.longitude, result.latitude)
-    context.searchCoordinates = [result.longitude, result.latitude]
-    console.log(context)
-    console.log(`http://api.openweathermap.org/data/2.5/weather?lat=${context.searchCoordinates[1]}&lon=${context.searchCoordinates[0]}&APPID=${process.env.REACT_APP_WEATHER}`)
+    context.searchCoordinates = [result.latitude, result.longitude]
     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${context.searchCoordinates[0]}&lon=${context.searchCoordinates[1]}&APPID=${process.env.REACT_APP_WEATHER}`)
     .then(response => response.json())
     .then(response => { 
-      console.log(response)
-      console.log(response.main.temp, response.main.humidity)
       setTemp(response.main.temp)
       setHumidity(response.main.humidity)
       calculTHI()
@@ -30,7 +29,18 @@ function Home() {
   const calculTHI = () => {
     setThi((1.8*temp/100) + (humidity/100) * (temp/100 - 14.4) +46.4)
    }
-
+   
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(Date.now())
+    console.log(context.searchCoordinates[0],context.searchCoordinates[1] )
+    fetch(`http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${context.searchCoordinates[0]}&lon=${context.searchCoordinates[1]}&dt=${1638626181}&APPID=${process.env.REACT_APP_WEATHER}`)
+    .then(response => response.json())
+    .then(res =>{ console.log(res)
+    
+    })
+    // fetch(`http://localhost:3000/`)
+  }
   
   return (
     <main className={classes.home}>
@@ -54,6 +64,11 @@ function Home() {
       <>
       <h3>THI :</h3>
       <ReactSpeedometer minValue={0} maxValue={100} segmentColors={['#3FBF50', '#7FC211', '#F2E311', '#F57710', '#C40D00']} value={thi}/>
+      THI en date du : 
+      <form onSubmit={handleSubmit}>
+        <input type="date" id="date" name="date" value={date ? date : "2019-06-01"} min="2018-01-01" max="2021-12-31" onChange={e => setDate(e.target.value)}/>
+        <button type="submit">Voir l'historique</button>
+       </form>
       </>
       :
       false }
