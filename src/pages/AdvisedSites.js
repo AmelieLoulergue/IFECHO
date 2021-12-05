bf6f38377437f848a9dd2d055316afb892c68add
jonathan justman <jojo.justman@gmail.com>
2021-12-05 02:26:56 +0100

    create dynamic page farm

--- src/pages/AdvisedSites.js
+++ src/pages/AdvisedSites.js
@@ -1,40 +1,32 @@
-import { Modal } from "bootstrap";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const AdvisedSites = () => {
-  const [modal, setModal] = useState({
-    card1: false,
-    card2: false,
-    card3: false,
-    card4: false,
-  });
+  const navigate= useNavigate()
 
-  const modalClickHandler = (field) => {
-    setModal({ ...modal, [field]: !modal[field] });
-  };
+  const redirect = (farmName)=>{
+    navigate(`/mes-elevages-conseilles/${farmName}`)
+  }
 
   return (
     <main>
-      <h1>{modal.card1.toString()}</h1>
       <h2>Mes élevages suivis</h2>
       <div className="grid-farms">
-        <div className="farm ok" onClick={() => modalClickHandler("card1")}>
+        <div className="farm ok" onClick={()=>redirect("blanche-maison")} >
           <h3>BLANCHE MAISON</h3>
           <p>CT : 0</p>
           <p>THI : 24.7</p>
           <button className="ok">Détail</button>
           {/* {modal.card1 && <Modal/>} */}
         </div>
-        <div className="farm danger" onClick={() => modalClickHandler("card2")}>
+        <div className="farm danger" onClick={()=>redirect("trevarez")} >
           <h3>TREVAREZ</h3>
           <p>CT : 15</p>
           <p>THI : 72.3</p>
           <button className="danger">Détail</button>
         </div>
         <div
-          className="farm warning"
-          onClick={() => modalClickHandler("card3")}
-        >
+          className="farm warning" onClick={()=>redirect("derval")} >
           <h3>DERVAL</h3>
           <p>CT : 7</p>
           <p>THI : 46.1</p>
@@ -42,8 +34,8 @@ const AdvisedSites = () => {
         </div>
         <div
           className="farm warning"
-          onClick={() => modalClickHandler("card4")}
-        >
+         
+          onClick={()=>redirect("trinottieres")} >
           <h3>TRINOTTIERES</h3>
           <p>CT : 5</p>
           <p>THI : 39.3</p>
